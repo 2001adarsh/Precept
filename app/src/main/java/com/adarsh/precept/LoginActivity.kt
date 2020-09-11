35bb1939f52ecd67d37ec7f96f66ee8a54b10f4b
Adarsh Singh <2001adarshsingh@gmail.com>
2020-09-11 13:34:39 +0530

    Re commit

--- app/src/main/java/com/adarsh/precept/LoginActivity.kt
+++ app/src/main/java/com/adarsh/precept/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.adarsh.precept
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        editTextPhone.addTextChangedListener {
+            send_otp.isEnabled = !(it.isNullOrEmpty() || it.length<10 || it.length>10)
+            if(send_otp.isEnabled){
+                send_otp.backgroundTintList = ContextCompat.getColorStateList(this, R.color.white)
+                send_otp.isClickable = true
+            }else{
+                send_otp.backgroundTintList = ContextCompat.getColorStateList(this, R.color.Fadedwhite)
+                send_otp.isClickable = false
+            }
+        }
+
     }
 }
