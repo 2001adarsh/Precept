13e821ac52e596b66b6ef1647fd449828bec1483
Adarsh Singh <2001adarshsingh@gmail.com>
2020-09-25 16:51:51 +0530

    The Cloud Storage of the Firebase is used to create an instance of the user and userPhoto's is saved in a different Storage placed named as Uploads. The account creation part is done. Now Next task is of MainActivity part. Happy Coding.

--- app/src/main/java/com/adarsh/precept/ProfileSetUpActivity.kt
+++ app/src/main/java/com/adarsh/precept/ProfileSetUpActivity.kt
@@ -2,18 +2,22 @@ package com.adarsh.precept
 
 import android.Manifest
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_profile_set_up.*
@@ -22,13 +26,16 @@ class ProfileSetUpActivity : AppCompatActivity() {
 
     val storage by lazy { FirebaseStorage.getInstance() }
     val auth by lazy { FirebaseAuth.getInstance() }
+    val database by lazy { FirebaseFirestore.getInstance() }
     lateinit var downloadLink: String
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_set_up)
 
-        floatingActionButton.setOnClickListener {
+        failure_txt.visibility = View.INVISIBLE
+        changeImage.setOnClickListener {
             //First check for the Access of Internal Storage.
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
                 openGalleryToChoosePicture()
@@ -43,6 +50,32 @@ class ProfileSetUpActivity : AppCompatActivity() {
                     Toast.makeText(this, "Do not have permission to select photos!", Toast.LENGTH_SHORT).show()
             }
         }
+
+        next_button.setOnClickListener {
+            val name: String = person_name.text.toString()
+            if(name.length < 3){
+                person_name.error = "Atleast 3 letter should be there."
+            }else if(!::downloadLink.isInitialized){ // if picture not uploaded.
+                failure_txt.visibility = View.VISIBLE
+            }else{
+                next_button.isEnabled = false
+                progressDialog = createProgressDialog("Creating your account.", false)
+                progressDialog.show()
+                val user = User(name, downloadLink, downloadLink, auth.uid!!)
+                database.collection("users").document(auth.uid!!).set(user)
+                    .addOnSuccessListener {
+                        //Next Page
+                        startActivity(Intent(this, MainActivity::class.java)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+                    }.addOnFailureListener{
+                        //Failed to Upload.
+                        progressDialog.cancel()
+                        next_button.isEnabled = true
+                        failure_txt.visibility = View.VISIBLE
+                    }
+            }
+        }
+
     }
 
     private fun openGalleryToChoosePicture(){
@@ -65,6 +98,7 @@ class ProfileSetUpActivity : AppCompatActivity() {
 
     private fun uploadImage(it: Uri) {
         next_button.isEnabled = false
+        uploading.visibility = View.VISIBLE
         val ref = storage.reference.child("uploads/" + auth.uid.toString())
         val uploadTask = ref.putFile(it)
 
@@ -79,10 +113,10 @@ class ProfileSetUpActivity : AppCompatActivity() {
         }).addOnCompleteListener { task->
             if(task.isSuccessful){
                 next_button.isEnabled = true
+                uploading.visibility = View.INVISIBLE
                 downloadLink = task.result.toString()
                 Log.d("Profile Image", "uploadImage: $downloadLink" )
             }
-
         }.addOnFailureListener{
             failure_txt.isVisible = true
         }
---
+++ app/src/main/java/com/adarsh/precept/User.kt
@@ -0,0 +1,18 @@
+package com.adarsh.precept
+
+import com.google.firebase.firestore.FieldValue
+
+data class User(
+    val name: String,
+    val imageUrl: String,
+    val thumbImage: String,
+    val uid: String,
+    val deviceToken: String,
+    val status: String,
+    val onlineState: String
+){
+    constructor():this("", "","","","","", "")
+    constructor(name: String, imageUrl: String, thumbImage: String, uid: String):
+            this(name, imageUrl,thumbImage,uid,"","Hey There I'm using Precept", "")
+
+}
