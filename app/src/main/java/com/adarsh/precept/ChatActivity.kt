7c0b11f8ed259d7bbf2a6f0829d5cc192cae5879
Adarsh Singh <2001adarshsingh@gmail.com>
2020-10-04 11:41:16 +0530

    Set up Chat activity

--- app/src/main/java/com/adarsh/precept/ChatActivity.kt
+++ app/src/main/java/com/adarsh/precept/ChatActivity.kt
@@ -2,14 +2,53 @@ package com.adarsh.precept
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adarsh.precept.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
+import com.vanniktech.emoji.EmojiManager
+import com.vanniktech.emoji.google.GoogleEmojiProvider
+import kotlinx.android.synthetic.main.activity_chat.*
 
 const val UID = "uid"
 const val NAME = "name"
 const val IMAGE = "photo"
 
 class ChatActivity : AppCompatActivity() {
+    private val friendUid:String by lazy{
+        intent.getStringExtra(UID)
+    }
+    private val friendName:String by lazy {
+        intent.getStringExtra(NAME)
+    }
+    private val friendImage:String by lazy{
+        intent.getStringExtra(IMAGE)
+    }
+    private val currentUID:String by lazy {
+        FirebaseAuth.getInstance().uid!!
+    }
+    private val db:FirebaseDatabase by lazy {
+        FirebaseDatabase.getInstance()
+    }
+    lateinit var currentUser: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        EmojiManager.install(GoogleEmojiProvider()) //Install the emoji's before inflating the xml file.
         setContentView(R.layout.activity_chat)
+
+        //current user object
+        FirebaseFirestore.getInstance().collection("users").document(currentUID).get()
+            .addOnSuccessListener {
+                currentUser = it.toObject(User::class.java)!!
+            }
+
+        //Set up Friends image and Name in Toolbar
+        friend_name.text = friendName
+        Picasso.get().load(friendImage).placeholder(R.drawable.defaultimage).error(R.drawable.defaultimage).into(friend_image)
+
+
     }
 }
---
+++ app/src/main/java/com/adarsh/precept/models/Inbox.kt
@@ -0,0 +1,15 @@
+package com.adarsh.precept.models
+
+import java.util.*
+
+data class Inbox(
+    val msg:String,
+    val from:String,
+    var name:String,
+    var image:String,
+    val time:Date = Date(),
+    var count:Int = 0
+){
+    //Empty constructor for firebase
+    constructor():this("", "", "", "", Date(), 0)
+}
